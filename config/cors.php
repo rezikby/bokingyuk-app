f1c1add3f57f68ae42651c0b602d67ce662e9780
rezi <emailko@gmail.com>
2026-05-17 14:12:25 +0700

    masuk

--- config/cors.php
+++ config/cors.php
@@ -13,9 +13,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        // 'http://192.168.67.86:3000',
+        'https://rezi.yopaaa.xyz',
         env('FRONTEND_URL', ''),
     ],
 
